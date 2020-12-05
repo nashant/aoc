01f9ea55bf499bb3e8b8e1515ebdbbbb5ec4aa13
Anthony Nash <anthony.nash@du.co>
2020-12-05 14:50:53 +0000

    Even enshortener day 5

--- 2020/5/main.py
+++ 2020/5/main.py
@@ -1,8 +1,6 @@
 from re import sub
-def binSub(binString):
-    return int(sub('[FL]', '0', sub('[BR]', '1', binString)), 2)
-
 with open('input.txt', 'r') as fd:
-    takenIds = sorted([binSub(l) for l in fd.read().split('\n') if l])
-    print(takenIds[-1])
-    print(set(range(takenIds[0], takenIds[-1] - takenIds[0])) - set(takenIds))
+    taken = [l for l in fd.read().split('\n') if l]
+    ids = sorted([int(sub('[FL]', '0', sub('[BR]', '1', s)), 2) for s in taken])
+    print(ids[-1])
+    print(set(range(ids[0], ids[-1] - ids[0])) - set(ids))
