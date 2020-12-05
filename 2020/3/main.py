060fdefa2f29eb467627da733e47f8be1f445591
Anthony Nash <anthony.nash@du.co>
2020-12-05 11:24:07 +0000

    Ensmallen day 3

--- 2020/3/main.py
+++ 2020/3/main.py
@@ -1,8 +1,7 @@
 from math import floor, prod
 with open('input.txt', 'r') as fd:
-    routes = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
+    routes = ((3, 1), (1, 1), (5, 1), (7, 1), (1, 2))
     lines = [l for l in fd.read().split('\n') if l]
-    maxH = max([h for h, v in routes])
-    lines = [(l * (1 + floor(((maxH + 1) * i + 1)/len(l)))) for i, l in enumerate(lines)]
-    paths = [[lines[i * v][i * h] for i in range(int(len(lines) / v))] for h, v in routes]
+    paths = [[lines[i*v][(i*h) % len(lines[i*v])] for i in range(int(len(lines)/v))] for h, v in routes]
+    print(paths[0].count('#'))
     print(prod([path.count('#') for path in paths]))
