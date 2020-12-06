0ef74a99197489a22d33ed540d2bc9b0e1f7bee8
Anthony Nash <anthony.nash@du.co>
2020-12-06 16:38:02 +0000

    Complete day 6

--- 2020/6/main.py
+++ 2020/6/main.py
@@ -1,2 +1,4 @@
 with open('input.txt', 'r') as fd:
-    lines = [l for l in fd.read().split('\n') if l]
+    lines = [l.strip().split('\n') for l in fd.read().split('\n\n') if l]
+    print(f"Part 1: {sum([len(set(''.join(l))) for l in lines])}")
+    print(f"Part 2: {sum([len(set.intersection(*[set(p) for p in l])) for l in lines])}")
