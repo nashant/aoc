7aae5d5954ba067b3cf1f5b581c7d753c84eeb78
Anthony Nash <anthony.nash@du.co>
2020-12-06 16:42:22 +0000

    Enshorten 6

--- 2020/6/main.py
+++ 2020/6/main.py
@@ -1,4 +1,3 @@
-with open('input.txt', 'r') as fd:
-    lines = [l.strip().split('\n') for l in fd.read().split('\n\n') if l]
-    print(f"Part 1: {sum([len(set(''.join(l))) for l in lines])}")
-    print(f"Part 2: {sum([len(set.intersection(*[set(p) for p in l])) for l in lines])}")
+lines = [l.strip().split('\n') for l in "".join(open('input.txt', 'r')).split('\n\n')]
+print(f"Part 1: {sum([len(set(''.join(l))) for l in lines])}")
+print(f"Part 2: {sum([len(set.intersection(*[set(p) for p in l])) for l in lines])}")
