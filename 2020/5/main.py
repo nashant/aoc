bc2ef423248c7c48d7dc5833a028c6f2f276e8eb
Anthony Nash <anthony.nash@du.co>
2020-12-06 16:37:42 +0000

    Extra enshorten 5

--- 2020/5/main.py
+++ 2020/5/main.py
@@ -1,6 +1,4 @@
 from re import sub
-with open('input.txt', 'r') as fd:
-    taken = [l for l in fd.read().split('\n') if l]
-    ids = sorted([int(sub('[FL]', '0', sub('[BR]', '1', s)), 2) for s in taken])
-    print(ids[-1])
-    print(set(range(ids[0], ids[-1] - ids[0])) - set(ids))
+ids = sorted([int(sub('[FL]', '0', sub('[BR]', '1', s)), 2) for s in open('input.txt', 'r')])
+print(ids[-1])
+print(set(range(ids[0], ids[-1] - ids[0])) - set(ids))
